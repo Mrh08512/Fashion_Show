2f252578fcf2087c1da721b6f39227a001d2ae92
TangJR <tangjr.work@gmail.com>
2015-07-12 00:30:43 +0800

    update plaza publish view

--- Fashion1/Fashion/Controller/Mian/MainVC/PlazaPublishViewController.m
+++ Fashion1/Fashion/Controller/Mian/MainVC/PlazaPublishViewController.m
@@ -11,7 +11,7 @@
 @interface PlazaPublishViewController ()
 
 @property (weak, nonatomic) UIImageView *largePhotoImageView;
-
+@property (weak, nonatomic) UIScrollView *scrollView;
 @property (strong, nonatomic) NSMutableArray *photos;
 
 @end
@@ -23,16 +23,34 @@
     // Do any additional setup after loading the view.
     
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self setupUI];
 }
 
 #pragma mark - setup
 
-- (void)setupPhotos {
+- (void)setupUI {
+    
+    UIScrollView *scrollView = [UIScrollView new];
+    scrollView.contentSize = CGSizeMake(SCREEN_WIDTH, 1000);
+    self.scrollView = scrollView;
+    
+    [scrollView mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.edges.mas_equalTo(UIEdgeInsetsMake(0, 0, 0, 0));
+    }];
     
     UIImageView *largePhotoImageView = [UIImageView new];
-    largePhotoImageView.width = SCREEN_WIDTH;
-    largePhotoImageView.height = 200;
+    largePhotoImageView.backgroundColor = [UIColor redColor];
     [self.view addSubview:largePhotoImageView];
+    self.largePhotoImageView = largePhotoImageView;
+    
+    [largePhotoImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.top.mas_equalTo(64);
+        make.left.and.right.mas_equalTo(0);
+        make.height.mas_equalTo(200);
+    }];
 }
 
 #pragma mark - Private methods
