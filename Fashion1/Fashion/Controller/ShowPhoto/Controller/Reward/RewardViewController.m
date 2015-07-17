ad616d34a6cda87c2c195fd83702004f57d9b660
TangJR <tangjr.work@gmail.com>
2015-07-17 16:58:40 +0800

    solve conflict

--- Fashion1/Fashion/Controller/ShowPhoto/Controller/Reward/RewardViewController.m
+++ Fashion1/Fashion/Controller/ShowPhoto/Controller/Reward/RewardViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "RewardViewController.h"
+#import "RewardListTableViewCell.h"
 
-@interface RewardViewController ()
+@interface RewardViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) UITableView *tableView;
 
 @end
 
@@ -19,11 +22,45 @@
     // Do any additional setup after loading the view.
     
     [self setupNavigation];
+    [self setupUI];
 }
 
 - (void)setupNavigation {
     
     self.navigationItem.title = @"悬赏";
+}
+
+- (void)setupUI {
+    
+    UITableView * tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
+    tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    tableView.backgroundColor = [UIColor clearColor];
+    tableView.showsVerticalScrollIndicator = NO;
+    [tableView registerNib:[UINib nibWithNibName:@"RewardListTableViewCell" bundle:nil] forCellReuseIdentifier:@"RewardListTableViewCell"];
+    [self.view addSubview:tableView];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    return [self configCellWithIndexPath:indexPath];
+}
+
+- (RewardListTableViewCell *)configCellWithIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString * const identifer = @"RewardListTableViewCell";
+    
+    RewardListTableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:identifer];
+    
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 1;
 }
 
 @end
---
+++ Fashion1/Fashion/Controller/ShowPhoto/View/Reward/RewardListTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  RewardListTableViewCell.h
+//  Fashion
+//
+//  Created by TangJR on 7/17/15.
+//  Copyright (c) 2015 MRH-MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RewardListTableViewCell : UITableViewCell
+
+@end
